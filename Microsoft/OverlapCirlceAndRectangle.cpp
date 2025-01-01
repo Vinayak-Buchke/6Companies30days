4bdc7d4fcfac4c359364a06a54153cbf6b49f478
Vinayak <vinayakkumar808@gmail.com>
2025-01-01 11:12:16 +0530

    Added OverlapCircleAndRectangle problem

---
+++ Microsoft/OverlapCirlceAndRectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+              int xn = max(x1, min(x2,xCenter));
+        int yn = max(y1, min(y2, yCenter));
+        return (yn-yCenter)*(yn-yCenter)+(xn-xCenter)*(xn-xCenter) <= radius*radius;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    cout<<obj.checkOverlap(1,0,0,1,-1,3,1);
+}
