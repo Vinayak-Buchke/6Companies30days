54f7646582fea4c51fa619daaccd2f7d2cee392c
Vinayak <vinayakkumar808@gmail.com>
2025-01-09 18:59:28 +0530

    Added  Problems

---
+++ Amazon/DeleteNNodes.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+struct Node {
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+class Solution {
+  public:
+    Node* linkdelete(Node* head, int n, int m) {
+        // code here
+        Node* node = head;
+        while(node!=nullptr)
+        {
+            Node* prev = nullptr;
+            // Skip m nodes
+            for(size_t id=0; id<m && node!=nullptr; id++)
+            {
+                prev = node;
+                node = node->next;
+            }
+            
+            // Delete n nodes
+            for(size_t id=0; id<n && node!=nullptr; id++)
+            {
+                Node* temp = node;
+                node = node->next;
+                delete temp;
+            }
+            prev->next = node;
+        }
+        return head;
+    }
+};
